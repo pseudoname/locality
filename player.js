5d43976cc2c0fe3c4a3a20ee5e1a128165cca3d4
pseudoname <palak.chokshi@gmail.com>
2014-10-09 14:58:14 -0700

    added setter method for view container

--- player.js
+++ player.js
@@ -2,6 +2,7 @@ var LocalityPlayer = function(map, viewContainerEle, options){
   var self = this;
   var _map = map;
   var _options = options || {};
+  var _viewContainer = viewContainerEle;
   if(!_options.position){
     _options = {
       position: _map.getCenter(),
@@ -11,8 +12,11 @@ var LocalityPlayer = function(map, viewContainerEle, options){
       }
     };
   }
+  self.setViewContainer = function(ele){
+    _viewContainer = ele;
+  }
   self.loadStreetView = function(){
-    var panorama = new google.maps.StreetViewPanorama(viewContainerEle, panoramaOptions);
+    var panorama = new google.maps.StreetViewPanorama(_viewContainerEle, panoramaOptions);
     _map.setStreetView(panorama);
   }
 }
