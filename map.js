4be3ec1739c7a4d2a8b9964bead6cd268f94b44a
pseudoname <palak.chokshi@gmail.com>
2014-10-09 14:34:39 -0700

    removed comments

--- map.js
+++ map.js
@@ -33,31 +33,5 @@ var Locality = function(mapEle, mapOptions){
           google.maps.event.addDomListener(element, eventName, handler);      
         };
         /**************** Properties **********************/
-        this.directions = _directions;
+        this['directions'] = _directions;
 }
-/*function initialize(mapEle) {
-        var mapOptions = {
-          center: { lat: -34.397, lng: 150.644},
-          zoom: 8
-        };
-        var map = new google.maps.Map(mapEle,
-            mapOptions);
-            var marker = new google.maps.Marker({
-            position: map.getCenter(),
-            map: map,
-            title: 'Click to zoom'
-        });
-
-        google.maps.event.addListener(map, 'center_changed', function() {
-    // 3 seconds after the center of the map has changed, pan back to the
-    // marker.
-            window.setTimeout(function() {
-            map.panTo(marker.getPosition());
-            }, 3000);
-        });
-
-        google.maps.event.addListener(marker, 'click', function() {
-            map.setZoom(8);
-            map.setCenter(marker.getPosition());
-        });
-      }*/
