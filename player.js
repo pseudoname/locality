b17dd1592b1da5a598bf198ab71d96d3cd1b184d
pseudoname <palak.chokshi@gmail.com>
2014-10-28 15:22:02 -0700

    prefetch panoramas

--- player.js
+++ player.js
@@ -33,109 +33,27 @@ var LocalityPlayer = function(map, viewContainerEle, options){
         positionChangedFn(_panorama.getPosition());
       }
     });
-   
+    var panoramas = [];
+    for(var i=0; i<route.overview_path.length; i++){
+      var pano = new google.maps.StreetViewPanorama(_viewContainer, _options);
+      pano.setPosition(route.overview_path[i]);
+      panoramas.push(pano);
+    }
     //stepThrough(0,0,0,route);
-    stepThroughPath(route, 0);
+    stepThroughPath(panoramas, 0);
   };
-  function stepThroughPath(route, currentStep){
-    if(currentStep >= route.overview_path.length){
+  function stepThroughPath(panos, currentStep){
+    if(currentStep >= panos.length){
       return;
     }
     setTimeout(function(){
       console.log('Current Step: ' + currentStep);
-      _panorama.setPosition(route.overview_path[currentStep]);
-      _map.setStreetView(_panorama);
+      
+      //_panorama.setPosition(route.overview_path[currentStep]);
+      _map.setStreetView(panos[currentStep]);
       currentStep++;
-      stepThroughPath(route, currentStep);
+      stepThroughPath(panos, currentStep);
     },_options.speed);
   }
-  function stepThrough(currentLeg, currentStep, currentPath, route){
-    
-    
-      
-        
-        if(currentLeg < route.legs.length){
-          console.log('Current Leg: ' + currentLeg)
-          console.log('Steps in Leg ' + currentLeg + ': ' + route.legs[currentLeg].steps.length);
-          if(currentStep < route.legs[currentLeg].steps.length){
-            console.log('Current Step: ' + currentStep);
-            console.log('Paths in Step ' + currentStep + ': ' + route.legs[currentLeg].steps[currentStep].path.length);
-            if(currentPath < route.legs[currentLeg].steps[currentStep].path.length){
-              console.log('Current Path: ' + currentPath);
-            }
-          }
-        }
-        
-                
-                var interval = setTimeout(function(){
-                  //if(i < route.legs.length){
-                    
-                    if(currentLeg < route.legs.length && currentStep < route.legs[currentLeg].steps.length && currentPath >= route.legs[currentLeg].steps[currentStep].path.length){
-                      currentPath = 0;
-                      
-                      if(currentLeg < route.legs.length && currentStep >= route.legs[currentLeg].steps.length){
-                        currentStep = 0;
-                        
-                        if(currentLeg >= route.legs.length){
-                          return;
-                        }
-                        else{
-                          _panorama.setPosition(route.legs[currentLeg].steps[currentStep].path[currentPath]);
-                          _map.setStreetView(_panorama);
-                          currentLeg++;
-                        }
-                      }
-                      else{
-                        if(currentLeg >= route.legs.length){
-                          return;
-                        }
-                        else{
-                          _panorama.setPosition(route.legs[currentLeg].steps[currentStep].path[currentPath]);
-                          _map.setStreetView(_panorama);
-                          currentLeg++;
-                        }
-                        currentStep++;
-                      }
-                    }
-                    else{
-                      if(currentLeg < route.legs.length && currentStep >= route.legs[currentLeg].steps.length){
-                        currentStep = 0;
-                        
-                        if(currentLeg >= route.legs.length){
-                          return;
-                        }
-                        else{
-                          _panorama.setPosition(route.legs[currentLeg].steps[currentStep].path[currentPath]);
-                          _map.setStreetView(_panorama);
-                          currentLeg++;
-                        }
-                      }
-                      else{
-                        if(currentLeg >= route.legs.length){
-                          return;
-                        }
-                        else{
-                          _panorama.setPosition(route.legs[currentLeg].steps[currentStep].path[currentPath]);
-                          _map.setStreetView(_panorama);
-                          currentLeg++;
-                        }
-                        currentStep++;
-                      }
-                      //_panorama.setPosition(route.legs[currentLeg].steps[currentStep].path[currentPath]);
-                      //_map.setStreetView(_panorama);
-                      currentPath++;
-                    }
-                    
-                    
-                    stepThrough(currentLeg, currentStep, currentPath, route);
-                  //}
-                  //clearTimeout(interval);
-                },_options.speed);
-              
-            
-          
-        
-      
-    
-  }
+  
 }
