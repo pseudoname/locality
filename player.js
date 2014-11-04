ea8205adb170a5a68b786d437832fca873de6cb6
pseudoname <palak.chokshi@gmail.com>
2014-11-04 14:55:35 -0800

    option setting changed

--- player.js
+++ player.js
@@ -2,44 +2,29 @@ var LocalityPlayer = function(map, viewContainerEle, options){
   var self = this;
   var _map = map;
   var _panorama;
-  var _options = options || {};
+  var _options = MergeRecursive(_options,options);
   var _viewContainer = viewContainerEle;
   var _currentStep = 0;
   var _currentRoute = [];
   
-  if(!_options.position){
+  if(!_options){
     _options = {
-      position: _map.getCenter(),
-      pov: {
-        heading: 34,
-        pitch: 0
+      panoOptions:{
+        position: _map.getCenter(),
+        pov: {
+          heading: 34,
+          pitch: 0
+        }
       },
       speed:1000
     };
   }
-  _panorama = new google.maps.StreetViewPanorama(_viewContainer, _options);
+  _panorama = new google.maps.StreetViewPanorama(_viewContainer, _options.panoOptions);
   _map.setStreetView(_panorama);
   self.addPanoramaEvent = function(eventName, handler){
     google.maps.event.addListener(_panorama, eventName, handler);
   };
-  /*self.addPanoramaEvent('pano_changed', function(){
-    if(_currentStep > -1 && _currentStep >= _currentRoute.length){
-      _currentStep = 0;
-      _currentRoute = [];
-      if(_options.playEnded){
-        _options.playEnded();
-      }
-      return;
-    }
-    _map.setStreetView(_panorama);
-    _currentStep++;
-    console.log("Street View set on map for step " + _currentStep-1);
-    self.setPosition(_currentRoute[_currentStep-1]);
-    console.log('Status of set position for Pano ID ' + _panorama.getPano() + ' is ' + _panorama.getStatus());
-      
-      
-      
-  });*/
+  
   self.setViewContainer = function(ele){
     _viewContainer = ele;
   };
@@ -48,8 +33,7 @@ var LocalityPlayer = function(map, viewContainerEle, options){
     _map.setStreetView(_panorama);
   };
   self.setPosition = function(position){
-    _options.position = position;
-    
+    _options.panoOptions.position = position;
     _panorama.setPosition(position);
     if(_currentRoute && _currentRoute.length > 0 && _currentStep > 0 && _options.streetViewChanged){
         _options.streetViewChanged(_currentRoute[_currentStep-1]);
@@ -57,27 +41,14 @@ var LocalityPlayer = function(map, viewContainerEle, options){
     _map.setStreetView(_panorama);
   };
   self.play = function(route){
-    var panoramas = [];
-    for(var i=0; i<route.overview_path.length; i++){
-      var panoOptions = {
-        pov: _options.pov,
-        speed:_options.speed
-      };
-      var pano = new google.maps.StreetViewPanorama(_viewContainer, panoOptions);
-      pano.setPosition(route.overview_path[i]);
-      panoramas.push(pano);
-    }
     _currentRoute = route.overview_path;
-    //self.setPosition(_currentRoute[_currentStep]);
-    //stepThrough(0,0,0,route);
-    
     stepThroughPath(route, 0);
-    
-    //self.setPosition(route.overview_path[3]);
   };
+  
   function stepThroughPath(route, currentStep){
-    
+    _currentStep = currentStep;
     if(currentStep >= route.overview_path.length){
+      _currentStep = 0;
        return;
      }
     setTimeout(function(){
@@ -88,5 +59,30 @@ var LocalityPlayer = function(map, viewContainerEle, options){
       stepThroughPath(route, currentStep);
     },_options.speed);
   }
+  /*
+  * Recursively merge properties of two objects 
+  */
+  function MergeRecursive(obj1, obj2) {
+    if(obj2 == null || obj2 == undefined){
+      return obj1;
+    }
+    for (var p in obj2) {
+      try {
+        // Property in destination object set; update its value.
+        if ( obj2[p].constructor==Object ) {
+          obj1[p] = MergeRecursive(obj1[p], obj2[p]);
   
+        } else {
+          obj1[p] = obj2[p];
+  
+        }
+  
+      } catch(e) {
+        // Property in destination object not set; create it and set its value.
+        obj1[p] = obj2[p];
+  
+      }
+    }
+  return obj1;
+  }
 }
